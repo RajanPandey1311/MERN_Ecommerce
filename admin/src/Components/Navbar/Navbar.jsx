a8dd8e5216c1642fcbe2df5cf5247995a677fda7
Rajan Pandey <130823894+RajanPandey1311@users.noreply.github.com>
2024-04-13 12:42:33 +0530

    Update Navbar.jsx

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import "./Navbar.css";
 import navlogo from "../../assets/nav-logo.svg";
 import navProfile from "../../assets/nav-profile.svg";
+
 const Navbar = () => {
+  const handleBackToStore = () => {
+    window.location.href = "https://rajan-ecommerce.vercel.app/";
+  };
+
   return (
     <div className="navbar">
-      <img src={navlogo} className="nav-logo" alt="" />
-      <img src={navProfile} className="nav-profile" alt="" />
+      <img src={navlogo} className="nav-logo" alt="Logo" />
+      <div className="navbar-buttons">
+        <button className="back-to-store-btn" onClick={handleBackToStore}>
+          Back to Store
+        </button>
+        <img src={navProfile} className="nav-profile" alt="Profile" />
+      </div>
     </div>
   );
 };
